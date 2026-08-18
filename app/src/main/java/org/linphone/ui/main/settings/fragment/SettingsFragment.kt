142d710071040f4b60324841e64bf46c1cebb8a7
compws <6.0542534e+07+compws@users.noreply.github.com>
2026-08-18 19:43:13 +0000

    feat(KID-282): remove Android call-redirection feature

--- app/src/main/java/org/linphone/ui/main/settings/fragment/SettingsFragment.kt
+++ app/src/main/java/org/linphone/ui/main/settings/fragment/SettingsFragment.kt
@@ -19,10 +19,7 @@
  */
 package org.linphone.ui.main.settings.fragment
 
-import android.Manifest
 import android.app.Activity
-import android.app.role.RoleManager
-import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.net.Uri
@@ -32,7 +29,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.UiThread
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
@@ -56,7 +52,6 @@ class SettingsFragment : GenericMainFragment() {
         private const val TAG = "[Settings Fragment]"
 
         private const val RINGTONE_PICKER_INTENT_ID = 89
-        private const val REDIRECT_ROLE_REQUEST_CODE = 101
     }
 
     private lateinit var binding: SettingsFragmentBinding
@@ -202,15 +197,6 @@ class SettingsFragment : GenericMainFragment() {
                     val toastMessage = getString(R.string.settings_calls_change_ringtone_picker_unavailable_toast)
                     (requireActivity() as GenericActivity).showRedToast(toastMessage, R.drawable.warning_circle)
                 }
-            }
-        }
-
-        viewModel.callRedirectionServiceEnabledEvent.observe(viewLifecycleOwner) {
-            it.consume {
-                Log.i(
-                    "$TAG Call redirection service enabled, making sure we have the required permissions and role granted"
-                )
-                requestCallRedirectionRole()
             }
         }
 
@@ -409,34 +395,4 @@ class SettingsFragment : GenericMainFragment() {
         dialog.show()
     }
 
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        for (permission in permissions) {
-            val isGranted = permission.value
-            if (isGranted) {
-                Log.i("$TAG [${permission.key}] permission has been granted")
-            } else {
-                Log.w("$TAG [${permission.key}] permission has been denied!")
-            }
-        }
-    }
-
-    private fun requestCallRedirectionRole() {
-        requestPermissionLauncher.launch(arrayOf(Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_PHONE_NUMBERS))
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            val roleManager: RoleManager = requireContext().getSystemService(Context.ROLE_SERVICE) as RoleManager
-            // Check if the app needs to register call redirection role.
-            val shouldRequestRole = roleManager.isRoleAvailable(RoleManager.ROLE_CALL_REDIRECTION) &&
-                    !roleManager.isRoleHeld(RoleManager.ROLE_CALL_REDIRECTION)
-            if (shouldRequestRole) {
-                Log.i("$TAG Starting intent to request Linphone be used as Call Redirection")
-                val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_REDIRECTION)
-                startActivityForResult(intent, REDIRECT_ROLE_REQUEST_CODE)
-            } else {
-                Log.i("$TAG Linphone is already defined as Call Redirection")
-            }
-        }
-    }
 }
--- app/src/main/java/org/linphone/ui/main/settings/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/org/linphone/ui/main/settings/viewmodel/SettingsViewModel.kt
@@ -93,11 +93,6 @@ class SettingsViewModel
 
     val showRingtonePickerEvent = MutableLiveData<Event<Uri?>>()
 
-    val callRedirectionService = MutableLiveData<Boolean>()
-    val callRedirectionServiceEnabledEvent: MutableLiveData<Event<Boolean>> by lazy {
-        MutableLiveData<Event<Boolean>>()
-    }
-
     // Conversations settings
     val showConversationsSettings = MutableLiveData<Boolean>()
 
@@ -353,7 +348,6 @@ class SettingsViewModel
             vibrateDuringIncomingCall.postValue(core.isVibrationOnIncomingCallEnabled)
             autoRecordCalls.postValue(corePreferences.automaticallyStartCallRecording)
             useSmffForCallRecording.postValue(corePreferences.callRecordingUseSmffFormat)
-            callRedirectionService.postValue(corePreferences.useCallRedirectionService)
 
             useWifiOnly.postValue(core.isWifiOnlyEnabled)
             allowIpv6.postValue(core.isIpv6Enabled)
@@ -558,18 +552,6 @@ class SettingsViewModel
         coreContext.postOnCoreThread { core ->
             core.ring = ringtone.toString()
             Log.i("$TAG Newly set ringtone is [${core.ring}]")
-        }
-    }
-
-    @UiThread
-    fun toggleCallRedirectionService() {
-        val newValue = callRedirectionService.value == false
-        if (newValue) {
-            callRedirectionServiceEnabledEvent.postValue(Event(true))
-        }
-        coreContext.postOnCoreThread {
-            corePreferences.useCallRedirectionService = newValue
-            callRedirectionService.postValue(newValue)
         }
     }
 
