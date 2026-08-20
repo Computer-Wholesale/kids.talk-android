ef733a3599c03ab267c6d311c2e687e504489ff3
compws <6.0542534e+07+compws@users.noreply.github.com>
2026-08-20 06:23:02 +0000

    test(KID-394): complete deterministic instrumented acceptance matrix

--- app/src/androidTest/java/org/linphone/ui/main/kidstalk/KidsTalkCallFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/org/linphone/ui/main/kidstalk/KidsTalkCallFragmentInstrumentedTest.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.isNotDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.not
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -31,6 +34,83 @@ class KidsTalkCallFragmentInstrumentedTest {
     }
 
     @Test
+    fun validLocalContactSavesAndShowsCallView() {
+        ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
+            Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
+                .putExtra("has_contact", false)
+        ).use {
+            onView(withId(org.linphone.R.id.setup_name_input)).perform(replaceText("Household contact"))
+            onView(withId(org.linphone.R.id.setup_number_input)).perform(replaceText("512345"), closeSoftKeyboard())
+            onView(withId(org.linphone.R.id.save_contact_button)).check(matches(isEnabled())).perform(click())
+            onView(withId(org.linphone.R.id.call_view)).check(matches(isDisplayed()))
+            onView(withId(org.linphone.R.id.call_button)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun authorisedCredentialAllowsLocalContactChange() {
+        ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
+            Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
+                .putExtra("has_contact", true)
+                .putExtra("credential_result", "authorized")
+        ).use {
+            onView(withId(org.linphone.R.id.change_contact_button)).perform(click())
+            onView(withId(org.linphone.R.id.setup_view)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun noDeviceCredentialShowsSetupAfterNeutralNoticePath() {
+        ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
+            Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
+                .putExtra("has_contact", true)
+                .putExtra("credential_result", "no_device_credential")
+        ).use {
+            onView(withId(org.linphone.R.id.change_contact_button)).perform(click())
+            onView(withId(org.linphone.R.id.setup_view)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun cancelledCredentialKeepsExistingCallView() {
+        ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
+            Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
+                .putExtra("has_contact", true)
+                .putExtra("credential_result", "cancelled")
+        ).use {
+            onView(withId(org.linphone.R.id.change_contact_button)).perform(click())
+            onView(withId(org.linphone.R.id.call_view)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun oneTapCallUsesValidatedHouseholdExtension() {
+        val scenario = ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
+            Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
+                .putExtra("has_contact", true)
+        )
+        scenario.use {
+            onView(withId(org.linphone.R.id.call_button)).perform(click())
+            scenario.onActivity { activity ->
+                assertEquals("512345", activity.lastValidatedCallExtension)
+            }
+        }
+    }
+
+    @Test
+    fun managedContactRemovalFallsBackToLocalSetup() {
+        // I-05: The host models the post-MDM-removal state with neither a managed nor local contact.
+        ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
+            Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
+                .putExtra("has_contact", false)
+                .putExtra("managed", false)
+        ).use {
+            onView(withId(org.linphone.R.id.setup_view)).check(matches(isDisplayed()))
+            onView(withId(org.linphone.R.id.call_view)).check(matches(not(isDisplayed())))
+        }
+    }
+
+    @Test
     fun managedContactHidesChangeControl() {
         ActivityScenario.launch<KidsTalkFragmentTestHostActivity>(
             Intent(ApplicationProvider.getApplicationContext(), KidsTalkFragmentTestHostActivity::class.java)
@@ -38,7 +118,7 @@ class KidsTalkCallFragmentInstrumentedTest {
                 .putExtra("managed", true)
         ).use {
             onView(withId(org.linphone.R.id.call_view)).check(matches(isDisplayed()))
-            onView(withId(org.linphone.R.id.change_contact_button)).check(matches(isNotDisplayed()))
+            onView(withId(org.linphone.R.id.change_contact_button)).check(matches(not(isDisplayed())))
             onView(withId(org.linphone.R.id.call_button)).check(matches(isDisplayed()))
         }
     }
